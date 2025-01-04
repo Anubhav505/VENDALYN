26523505ae3bdb230980e3a86da9b373350dc8d0
Anubhav <xcaliber704@gmail.com>
2025-01-05 03:30:19 +0530

    try try but don't cry

--- src/app/api/createOrder/route.ts
+++ src/app/api/createOrder/route.ts
@@ -2,21 +2,31 @@ import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 export async function POST(request: Request) {
-  const { amount } = await request.json();
-
-  const razorpay = new Razorpay({
-    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
-    key_secret: process.env.RAZORPAY_SECRET!,
-  });
-
   try {
+    const { amount } = await request.json();
+
+    const razorpay = new Razorpay({
+      key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+      key_secret: process.env.RAZORPAY_SECRET!,
+    });
+
     const options = {
       amount: amount,
       currency: "INR",
     };
+
     const order = await razorpay.orders.create(options);
     return NextResponse.json(order);
-  } catch (error) {
-    return NextResponse.json({ message : "hi" }, { status: 500 });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      // TypeScript now knows that `error` is an instance of `Error`
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    } else {
+      // Handle unexpected errors
+      return NextResponse.json(
+        { error: "An unexpected error occurred" },
+        { status: 500 }
+      );
+    }
   }
 }
