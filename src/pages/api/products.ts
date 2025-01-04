84c66ddd65a5f13f6f6ab0b3c641f194e2169074
Anubhav <xcaliber704@gmail.com>
2025-01-04 11:16:50 +0530

    test products api

--- src/pages/api/products.ts
+++ src/pages/api/products.ts
@@ -2,8 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import dbConnect from "@/lib/dbConnect";
 import Product from "@/models/Product";
 
-export default async function handler(req: NextApiRequest,res: NextApiResponse) {
-    await dbConnect();
-    req.method === "GET"
-    return res.json(await Product.find());
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await dbConnect();
+  if (req.method !== "GET") {
+    return res.json({ message: "Method is not recognized" });
+  }
+  const product = await Product.find();
+  return res.json(product);
 }
