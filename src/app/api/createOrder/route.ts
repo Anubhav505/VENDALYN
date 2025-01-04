04e7e6a954b9b557c9cd2d5a1446c61055eae487
Anubhav <xcaliber704@gmail.com>
2025-01-05 03:27:19 +0530

    try try but don't cry

--- src/app/api/createOrder/route.ts
+++ src/app/api/createOrder/route.ts
@@ -1,17 +1,22 @@
 import { NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
-const razorpay = new Razorpay({
-  key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID as string,
-  key_secret: process.env.RAZORPAY_SECRET_ID,
-});
+export async function POST(request: Request) {
+  const { amount } = await request.json();
 
-export async function POST(req: Request) {
-  const { amount } = await req.json();
-  const order = await razorpay.orders.create({
-    amount,
-    currency: "INR",
+  const razorpay = new Razorpay({
+    key_id: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID!,
+    key_secret: process.env.RAZORPAY_SECRET!,
   });
 
-  return NextResponse.json(order);
+  try {
+    const options = {
+      amount: amount,
+      currency: "INR",
+    };
+    const order = await razorpay.orders.create(options);
+    return NextResponse.json(order);
+  } catch (error) {
+    return NextResponse.json({ message : "hi" }, { status: 500 });
+  }
 }
