9169af1c03d372a6b35b4371c858b480ddc76dbf
Anubhav <xcaliber704@gmail.com>
2025-01-05 20:30:18 +0530

    logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,18 +1,19 @@
 import Link from "next/link";
 import NavIcon from "./NavIcon";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
-    <div className="sticky top-0 h-[10vh] flex justify-between items-center px-8 md:px-10 border-black border-b border-opacity-20 z-20 bg-white">
+    <div className="sticky top-0 h-[10vh] flex justify-between items-center px-8 md:px-10 border-black border-b border-opacity-20 z-20 bg-white overflow-hidden">
       <div></div>
-        <Link href="/" className="text-3xl font-semibold">
-          VENDALYN
-        </Link>
-        <div>
-            <NavIcon />
-        </div>
+      <Link href="/" className="h-full w-28 text-3xl font-semibold relative">
+        <Image src="/favicon.png" alt="logo" layout="fill" objectFit="cover" />
+      </Link>
+      <div>
+        <NavIcon />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
