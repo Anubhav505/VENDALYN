0be7c604100b11bace4d5714ba01b9b31c9115e8
Anubhav <xcaliber704@gmail.com>
2025-01-11 18:43:40 +0530

    almost

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,12 +5,13 @@ const page = () => {
         <div className='mt-12 flex items-center justify-center flex-col w-full'>
             <div className='w-[70%] text-justify font-normal leading-9'>
                 <h1 className='text-4xl mb-6  uppercase text-center leading-10'>
-                    "The journey to redefining convenience and quality is ongoing, and we’re excited to lead the way."
+                    "The journey to redefining convenience and quality is ongoing, and we&#39;re
+ excited to lead the way."
                 </h1>
-                <p className='mb-3 text-center'>At VENDALYN, we believe in a world where shopping is effortless, affordable, and fulfilling. As a one-stop destination, we pride ourselves on offering everything you need and more—whether it’s the everyday essentials or the little luxuries that brighten your life.</p>
+                <p className='mb-3 text-center'>At VENDALYN, we believe in a world where shopping is effortless, affordable, and fulfilling. As a one-stop destination, we pride ourselves on offering everything you need and more—whether it&#39;s the everyday essentials or the little luxuries that brighten your life.</p>
                 <p className='mb-3 text-center'>Our mission is simple: to make high-quality products accessible to everyone, no matter where you are. By working with reliable suppliers and focusing on customer satisfaction, we ensure that every item we offer meets your expectations and delivers value beyond the price tag.</p>
-                <p className="mb-3 text-center">At VENDALYN, we don’t just sell products—we build trust. Every purchase reflects our unwavering commitment to quality, convenience, and sustainability. From eco-conscious practices to a carefully curated selection of products that cater to diverse needs, we aim to create a shopping experience that supports both your lifestyle and the planet. With every choice you make, you become part of a larger movement toward responsible consumption and a better future.</p>
-                <p className='mb-3 text-center'>When you shop with VENDALYN, you’re not just buying products; you’re embracing a vision of better living. Let us make every step of your shopping journey seamless and rewarding.</p>
+                <p className="mb-3 text-center">At VENDALYN, we don&#39;t just sell products—we build trust. Every purchase reflects our unwavering commitment to quality, convenience, and sustainability. From eco-conscious practices to a carefully curated selection of products that cater to diverse needs, we aim to create a shopping experience that supports both your lifestyle and the planet. With every choice you make, you become part of a larger movement toward responsible consumption and a better future.</p>
+                <p className='mb-3 text-center'>When you shop with VENDALYN, you&#39;re not just buying products; you&#39;re embracing a vision of better living. Let us make every step of your shopping journey seamless and rewarding.</p>
             </div>
 
         </div>
