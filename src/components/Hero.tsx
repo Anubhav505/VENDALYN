4b3656bf3d3afe313d19dc935b9283612144e9c3
Anubhav <xcaliber704@gmail.com>
2025-01-13 19:17:06 +0530

    done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,13 +16,13 @@ export default function Hero() {
   }, []);
   return (
     <div className="relative w-full h-[calc(100vh-8vh)] mb-12 ">
-      <div className=" h-full w-full relative">
+      <div className=" h-full w-full relative overflow-hidden">
         {/* <video className="absolute top-0 left-0 h-full w-full object-cover sm:object-[center_34%]" loop muted autoPlay src="/hero.mp4"/> */}
-        <Image alt="hero image" fill={true} src='/hero.jpg' className="absolute top-0 left-0 h-full w-full object-cover object-right sm:object-[center_75%]"/>
+        <Image alt="hero image" fill={true} src='/hero.jpg' className="absolute top-0 left-0 h-full w-full object-cover object-right-bottom scale-[1.2] sm:object-[center_75%]"/>
       </div>
       <div className="px-2 heading absolute top-0 h-full w-full flex flex-col justify-center text-center">
        
-          <div className=" relative top-40 flex flex-col gap-3">
+          <div className=" relative -top-20 flex flex-col gap-3">
           <h1 className="text-4xl sm:text-7xl font-bold text-white">
             New Season Arrivals
           </h1>
