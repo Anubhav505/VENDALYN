1ef1d0cd884aa405b5333c6363d46d246bed9b80
Anubhav <xcaliber704@gmail.com>
2025-01-14 03:05:03 +0530

    done

--- src/app/contactUs/page.tsx
+++ src/app/contactUs/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
     <div className='h-[50vh] w-full flex flex-col justify-center items-center text-lg'>
       <div>
         <p>Contact us at </p>
-        <p>Company Address : Delhi Road, Kharkhoda (Sonipat), Ward No. : 6, Pin Code : 131402</p>
+        {/* <p>Company Address : Delhi Road, Kharkhoda (Sonipat), Ward No. : 6, Pin Code : 131402</p> */}
         <p>Email : <a href='mailto:vendalyn.store@gmail.com' className="text-indigo-600 hover:underline">vendalyn.store@gmail.com</a></p>
         <p className='flex gap-1'>Instagram : <a href='https://www.instagram.com/vendalyn_' target="_blank" className="text-pink-500"><Instagram /></a></p>
       </div>
