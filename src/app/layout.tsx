7c62dd8d1f1e675b539a3105914048fca5f25361
Anubhav <xcaliber704@gmail.com>
2025-01-13 19:58:33 +0530

    done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import Sale from "@/components/Sale";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -39,6 +40,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Sale />
         <Navbar />
         {children}
         <Footer />
---
+++ src/components/Sale.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SALE = () => {
+  return (
+      <div className='bg-black w-full text-white text-center text-xs items-center py-1'>Makar Sankranti Sale Live Now</div>
+  )
+}
+
+export default SALE
