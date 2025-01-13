b858161b6d94f11b956ff37e13efa4826943b328
Anubhav <xcaliber704@gmail.com>
2025-01-13 14:59:02 +0530

    done

--- src/app/shipping-policy/page.tsx
+++ src/app/shipping-policy/page.tsx
@@ -6,11 +6,10 @@ const page = () => {
     <div className='h-full w-full flex justify-center my-12 px-2'>
       <div className='sm:w-[60%] flex flex-col gap-6 text-justify text-sm'>
         <h1 className='heading text-[5.5vw] font-bold sm:text-5xl text-center'>SHIPPING POLICY</h1>
-      <p><b>This does not apply for items in Pre-order- Pre-order items take 2-6 days to dispatch post the pre-order ends.</b></p>
 
-      <p><b>There might be rare cases of delays. (details for those will be mentioned in the description)</b></p>
+      <p><b>There might be rare cases of delays.</b></p>
 
-      <p>Your product is shipped within two business days after your order is placed. However, if your order is placed before 10 am, we are most likely to ship on the same day. Your order will be delivered 5-7 days after dispatch. Orders for T-Shirts and other clothing&apos;s will be processed in 1-2 business days ( as it is made on demand ) and will be delivered in 3-5 business days. You can track your order once it has been dispatched from our warehouse. An email and SMS will be sent with a link.</p>
+      <p>Your product will be shipped within 0-7 business days after your order is placed. Orders for T-shirts and other clothing items, which are made on demand, will be processed within 1-2 business days and delivered in 3-5 business days. Delivery of all orders typically occurs within 5-7 days after dispatch. You can track your order once it has been dispatched from our warehouse</p>
 
       <p>There might be instances of delays, in-case of any issue please use the <Link href='/contactUs' className="text-blue-500">CONTACT US</Link> Section to get in touch with the team.</p>
 
