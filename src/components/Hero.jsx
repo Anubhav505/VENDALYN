9a5999c84773eadad354ad820c7bebeff81600fd
Anubhav <xcaliber704@gmail.com>
2025-01-16 11:32:39 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,48 @@
+
+
 "use client";
 
-import ThreeD from "./ThreeD"; // Import the ThreeD component
+import { useEffect } from "react";
 
-export default function Hero() {
+import React from 'react'
+
+const Hero = () => {
+
+  useEffect(() => {
+    // Dynamically import the model-viewer script
+    const script = document.createElement("script");
+    script.type = "module";
+    script.src = "https://cdn.jsdelivr.net/npm/@google/model-viewer@latest";
+    document.head.appendChild(script);
+
+    // Cleanup when the component is unmounted
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className="relative h-[60vh] sm:h-[96vh]">
-      <div
-        id="three-container"
-        className="h-full sm:h-screen w-full overflow-hidden"
-        style={{
-          position: "relative",
-        }}
-      >
-        <ThreeD />
+    <div className="relative w-full h-[60vh] bg-black flex">
+      <div className="hidden sm:block h-full sm:w-[32.5%]"></div>
+      <div className="h-full w-full sm:w-[35%] flex items-center justify-center overflow-hidden">
+        <model-viewer
+          className=" h-full w-full"
+          src="/dragon_glass.glb"
+          alt="3D Model of Dragon Glass"
+          auto-rotate
+          camera-controls
+        ></model-viewer>
       </div>
-      <div className="h-full w-full absolute top-0 flex flex-col items-center justify-between py-3 text-white">
-        <h1 className="nav text-[7vw]">VENDALYN</h1>
-        <h1 className="nav text-[2.5vw]">BEYOND THE GLOOM</h1>
-      </div>
+      <div className="hidden sm:block h-full sm:w-[32.5%]"></div>
+      <h1 className="nav text-[7vw] absolute top-3 w-full text-white text-center">
+     VENDALYN
+      </h1>
+   
+      <h1 className="nav text-[2.5vw] absolute bottom-3 w-full text-white text-center">
+      BEYOND THE GLOOM
+      </h1>
     </div>
   );
 }
+
+export default Hero
