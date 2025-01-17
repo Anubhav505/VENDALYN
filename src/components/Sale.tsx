f8e017e5208569b8eabda4969b6d760fadfccdb3
Anubhav <xcaliber704@gmail.com>
2025-01-17 07:25:43 +0530

    done

--- src/components/Sale.tsx
+++ src/components/Sale.tsx
@@ -1,32 +1,27 @@
 "use client";
 import { Pause, Play } from "lucide-react";
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef, useState } from "react";
 
 const Music: React.FC<{ src?: string }> = ({ src = "/music/music.mp3" }) => {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [mounted, setMounted] = useState(false); // To track if the component is mounted
   const audioRef = useRef<HTMLAudioElement>(null);
 
-  useEffect(() => {
-    setMounted(true); // Set to true once the component is mounted on the client
-  }, []);
-
   const togglePlay = () => {
-    if (audioRef.current) {
-      isPlaying ? audioRef.current.pause() : audioRef.current.play();
+    const audio = audioRef.current;
+    if (audio) {
+      if (isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
       setIsPlaying(!isPlaying);
     }
   };
 
-  // Ensure the component is mounted before rendering
-  if (!mounted) {
-    return null; // Avoid rendering before the component is mounted on the client
-  }
-
   return (
-    <div className="flex gap-2 items-center fixed top-4 sm:top-5 left-5 z-[100]">
+    <div className="fixed top-4 left-5 z-[100] flex items-center gap-2">
       <button
-        className="text-purple-500 rounded-full border-purple-800 border p-1 flex items-center justify-center"
+        className="text-purple-500 border border-purple-800 rounded-full p-1"
         onClick={togglePlay}
       >
         {isPlaying ? <Pause /> : <Play />}
