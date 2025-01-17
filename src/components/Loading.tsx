5f15250488884aef43199758d48ea0013e9cd124
Anubhav <xcaliber704@gmail.com>
2025-01-17 20:10:19 +0530

    done

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,23 +4,17 @@ import React, { useEffect } from 'react';
 
 const Loading: React.FC = () => {
     useEffect(() => { 
-        gsap.from('.brand span', { 
-            duration: 1, 
-            y: "10vh", 
+        gsap.from('.brand h1', { 
+            duration: 1.5, 
+            y: "50vh", 
             opacity: 0,
             ease: "power2.out",
-            stagger: 0.2});
+            display: "none"
+       })
     }, []);
     return (
         <div className="fixed top-0 w-full flex items-center pt-1 justify-center h-screen bg-black text-white z-[9999999999999] brand">       
-                <span className='text-[7vw] nav'>V</span>
-                <span className='text-[7vw] nav'>E</span>
-                <span className='text-[7vw] nav'>N</span>
-                <span className='text-[7vw] nav'>D</span>
-                <span className='text-[7vw] nav'>A</span>
-                <span className='text-[7vw] nav'>L</span>
-                <span className='text-[7vw] nav'>Y</span>
-                <span className='text-[7vw] nav'>N</span>
+                <h1 className='text-[11vw] nav'>VENDALYN</h1>
         </div>
     );
 };
