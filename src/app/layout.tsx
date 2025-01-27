377da685a7b63c8fcc5b42fdaac24db5eb51a129
Anubhav <xcaliber704@gmail.com>
2025-01-27 20:26:15 +0530

    whatsapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Footer from "@/components/Footer";
+import Whatsapp from "@/components/Whatsapp";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,7 @@ export default function RootLayout({
       >
         {children}
         <Footer />
+        <Whatsapp/>
       </body>
     </html>
   );
--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -3,14 +3,10 @@ import React from 'react'
 
 const Whatsapp = () => {
     return (
-        <div className=' right-5 z-50 text-5xl rounded-lg overflow-hidden cursor-pointer flex  items-center px-1 gap-1'>
-            <a href="https://wa.me/message/V4YRKECJHD3TE1" target="_blank">
-                <i className="fa-brands fa-square-whatsapp text-green-500 p-1"></i>
+            <a href="https://wa.me/message/V4YRKECJHD3TE1" target="_blank" className='fixed bottom-5 right-5 z-50 text-5xl cursor-pointer'>
+                <i className="fa-brands fa-square-whatsapp text-green-500 "></i>
             </a>
-            <a href="https://www.instagram.com/vendalyn_/" target="_blank">
-                <Instagram color="#ff004c" size={49} className='' />
-            </a>
-        </div>
+
     )
 }
 
