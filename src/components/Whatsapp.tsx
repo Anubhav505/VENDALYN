8e40fa2c2f54ee7e9594437772a57221b525fe44
Anubhav <xcaliber704@gmail.com>
2025-01-27 21:12:07 +0530

    cod

--- src/components/Whatsapp.tsx
+++ src/components/Whatsapp.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Whatsapp = () => {
     return (
             <a href="https://wa.me/message/V4YRKECJHD3TE1" target="_blank" 
-            className='fixed bottom-5 right-5 z-50 text-5xl cursor-pointer shadow-[10px_10px_15px_5px_rgba(0,0,0,0.2)] px-1 rounded-lg   '>
+            className='fixed bottom-5 right-5 z-50 text-5xl cursor-pointer shadow-[5px_5px_10px_rgba(0,0,0,0.2)] p-1 rounded-lg flex items-center justify-center'>
                 <i className="fa-brands fa-square-whatsapp text-green-500"></i>
             </a>
 
