0135ad17a683d680bda3f587b3923f6d1321c576
Anubhav <xcaliber704@gmail.com>
2025-02-01 15:14:48 +0530

    done

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,22 @@
 export default function Hero() {
 return(
 <div className="sticky top-0 w-full overflow-hidden text-white -z-50">
+    <video src="https://res.cloudinary.com/daexpmksd/video/upload/v1738254774/site_axiy60.mp4"
+    className="object-cover w-full block sm:hidden h-[93vh]" 
+        loop 
+        muted 
+        autoPlay
+        playsInline/>
+
     <video 
         src="/hero.mp4" 
-        className="object-cover sm:h-[93vh] w-full" 
+        className="object-cover sm:h-[93vh] w-full hidden sm:block" 
         loop 
         muted 
         autoPlay
         playsInline
     />
-    <h1 className="nav text-[2.5vw] absolute bottom-3 w-full text-center">
+    <h1 className="nav text-[2.5vw] absolute bottom-10 w-full text-center">
          IN THE 7<sup>th</sup> HEAVEN
       </h1>
 </div>
