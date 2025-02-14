d148bff2d484d0b063124ece3d1fb5c198ab34e8
Anubhav <xcaliber704@gmail.com>
2025-02-14 11:53:03 +0530

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,59 +1,99 @@
+"use client"
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react"
 
-export default function footer() {
+export default function Footer() {
   return (
-    <div className="h-[55vh] sm:h-screen bg-black w-full flex flex-col text-white z-[51]">
-      <div className="h-1/2 flex gap-4 sm:flex-row p-4 sm:p-10">
-        <div className="sm:w-1/2 hidden sm:block">
-          <h1 className="V text-white sm:text-[1.5vw] font-normal">Where Quality Meets Confidence.</h1>
+    <div className="footer h-[55vh] sm:h-screen bg-black w-full flex flex-col text-white z-[51] overflow-hidden">
+      <div className=" h-1/2 flex justify-between p-4 sm:p-10">
+
+        <div className=" hidden sm:block ">
+
+          <div className="h-full flex flex-col justify-between">
+            <div>
+              <h1 className="V text-white sm:text-[1.5vw] font-normal">
+                Where Quality Meets Confidence.
+              </h1>
+            </div>
+
+            <div>
+              <a href="mailto:vendalyn.store@gmail.com">
+                <span className="group relative overflow-hidden text-sm font-medium">
+                  vendalyn.store@gmail.com
+                  <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+                </span>
+              </a>
+            </div>
+
+            <div></div>
+            <div></div>
+          </div>
+          
         </div>
-        <div className="flex flex-row justify-between w-full gap-4 sm:gap-0 sm:w-1/2 text-sm font-medium">
-          <div className="text-2xl sm:font-medium sm:text-sm font-light sm:w-1/2 flex flex-col gap-1">
+
+        <div className="flex justify-between w-full sm:w-fit sm:gap-12 ">
+
+          <div className="sm:w-1/2 flex flex-col gap-1 text-2xl sm:text-sm font-light sm:font-medium">
             <Link href={"/"}>
               <span className="group relative overflow-hidden">
                 Home
-                <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+                <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
               </span>
             </Link>
             <Link href={"/about"}>
               <span className="group relative overflow-hidden">
                 About
-                <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+                <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
               </span>
             </Link>
             <Link href={"/contactUs"}><span className="group relative overflow-hidden">
               Contact
-              <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+              <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
             </span></Link>
             <Link href={"/policies"}>
               <span className="group relative overflow-hidden">
                 Policies
-                <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+                <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
               </span></Link>
             <Link href={"/return"}>
               <span className="group relative overflow-hidden">
                 Exchange
-                <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
+                <span className="absolute -bottom-1 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
               </span></Link>
           </div>
-          <div className="sm:w-1/2 flex flex-col">
-            <a href="https://www.instagram.com/vendalyn_/" className="overflow-hidden " target="_blank">
-              <span className="group relative">
-                Instagram <ArrowUpRight className="text-xs inline-block" />
+
+          <div className="sm:w-1/2 flex flex-col items-end sm:items-start text-sm font-medium">
+
+            <a href="https://www.instagram.com/vendalyn_/" className="overflow-hidden" target="_blank">
+              <span className="group relative flex">
+                Instagram <ArrowUpRight className="text-xs" />
                 <span className="absolute bottom-0 left-0 w-0 h-[1px] bg-white transition-all duration-500 group-hover:w-full"></span>
-              </span> </a>
+              </span>
+            </a>
+
             <a href="https://wa.me/message/V4YRKECJHD3TE1" className=" overflow-hidden " target="_blank">
-              <span className="group relative">
+              <span className="group relative flex">
                 WhatsApp <ArrowUpRight className="text-xs inline-block" />
                 <span className="absolute bottom-0 left-0 w-0 h-[0.5px] bg-white transition-all duration-500 group-hover:w-full"></span>
-              </span> </a>
+              </span>
+            </a>
+
+            <a href="mailto:vendalyn.store@gmail.com" className="sm:hidden overflow-hidden" target="_blank">
+              <span className="group relative flex">
+                vendalyn.store@gmail.com
+                <span className="absolute -bottom-1 left-0 w-0 h-[0.5px] bg-white transition-all duration-500 group-hover:w-full"></span>
+              </span>
+            </a>
+
             <h1 className="text-sm font-medium">© 2025</h1>
+          
           </div>
+
         </div>
+
       </div>
-      <div className="h-1/2  flex items-center justify-center">
-        <h1 className="V text-[13vw] font-normal">VENDALYN</h1>
+      <div className="mainbody h-1/2 flex items-end justify-center">
+        <h1 className="V text-7xl sm:text-[19vw] font-semibold">VENDALYN</h1>
       </div>
     </div>
   )
